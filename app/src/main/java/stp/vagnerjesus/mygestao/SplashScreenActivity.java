a9761eb8176607e1a3ac1d5f3e6de870a4052160
Vagner Bom Jesus <vagneripg@gmail.com>
2020-02-24 23:35:27 +0000

    Commit Initial Commit Splash Screen

---
+++ app/src/main/java/stp/vagnerjesus/mygestao/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package stp.vagnerjesus.mygestao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private static int tempoScreen = 4000;//Segunndo do Splash Screen
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        },tempoScreen);
+    }
+}
