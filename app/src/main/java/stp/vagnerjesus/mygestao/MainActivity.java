17da1d80e9537e8086716e3a4a15eb30c40e5ecc
Vagner Bom Jesus <vagneripg@gmail.com>
2020-02-26 09:35:38 +0000

    Commit Atualizar Commit Icon App

--- app/src/main/java/stp/vagnerjesus/mygestao/MainActivity.java
+++ app/src/main/java/stp/vagnerjesus/mygestao/MainActivity.java
@@ -1,10 +1,15 @@
 package stp.vagnerjesus.mygestao;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mSliderViewPager;
+    private LinearLayout mDotLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
