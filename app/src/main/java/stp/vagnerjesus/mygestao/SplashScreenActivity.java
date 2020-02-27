6107921f0aa855bf0883b8787cdcdb88261984c6
Vagner Bom Jesus <vagneripg@gmail.com>
2020-02-27 00:33:33 +0000

    Commit Atualizar

--- app/src/main/java/stp/vagnerjesus/mygestao/SplashScreenActivity.java
+++ app/src/main/java/stp/vagnerjesus/mygestao/SplashScreenActivity.java
@@ -6,25 +6,61 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class SplashScreenActivity extends AppCompatActivity {
-    private static int tempoScreen = 4000;//Segunndo do Splash Screen
+    //Variavel
+    Animation topAnim, bottomAnim;
+    ImageView imagem;
+    TextView logo, slogan;
+
+    private static int SPLASH_SCREEN = 5000;//Segunndo do Splash Screen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+       // this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
+
+
+
+       // topAnim =  AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        //bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+
+        //Hooks
+
+        imagem = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView);
+        slogan = findViewById(R.id.textView2);
+
+        imagem.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+
 
 
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(getApplicationContext(), IntroActivity.class));
-                finish();
-            }
-        },tempoScreen);
+          @Override
+          public void run() {
+              startActivity(new Intent(getApplicationContext(), IntroActivity.class));
+             finish();
+          }
+       },SPLASH_SCREEN);
+
+
+
+
+
     }
 }
