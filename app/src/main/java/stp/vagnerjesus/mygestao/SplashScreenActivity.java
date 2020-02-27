6c3b118e7f05dd2f5a78912da4e50c629f72ca71
Vagner Bom Jesus <vagneripg@gmail.com>
2020-02-27 01:07:42 +0000

    Commit Atualizar e Erros na Animation

--- app/src/main/java/stp/vagnerjesus/mygestao/SplashScreenActivity.java
+++ app/src/main/java/stp/vagnerjesus/mygestao/SplashScreenActivity.java
@@ -2,9 +2,12 @@ package stp.vagnerjesus.mygestao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -32,8 +35,8 @@ public class SplashScreenActivity extends AppCompatActivity {
 
 
 
-       // topAnim =  AnimationUtils.loadAnimation(this,R.anim.top_animation);
-        //bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        //topAnim =  AnimationUtils.loadAnimation(this,R.anim.top_animation);
+       // bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
 
 
         //Hooks
@@ -49,18 +52,22 @@ public class SplashScreenActivity extends AppCompatActivity {
 
 
 
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-          @Override
-          public void run() {
-              startActivity(new Intent(getApplicationContext(), IntroActivity.class));
-             finish();
-          }
-       },SPLASH_SCREEN);
 
 
-
-
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                //Call next screen
+                Intent intent = new Intent(SplashScreenActivity.this,IntroActivity.class);
+                // Attach all the elements those you want to animate in design
+                Pair[]pairs=new Pair[2];pairs[0]=new Pair<View, String>(imagem,"logo_image");pairs[1]=new Pair<View, String>(logo,"logo_text");
+                //wrap the call in API level 21 or higher
+                if(android.os.Build.VERSION.SDK_INT>=android.os.Build.VERSION_CODES.LOLLIPOP)
+                {ActivityOptions options=ActivityOptions.makeSceneTransitionAnimation(SplashScreenActivity.this,pairs);
+                    startActivity(intent,options.toBundle());
+                }
+            }
+        },SPLASH_SCREEN);
 
     }
 }
