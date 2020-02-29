604fd004f51c7daa1d4801708275657e38d48738
Vagner Bom Jesus <vagneripg@gmail.com>
2020-02-29 14:03:22 +0000

    Commit Validaçao Criar VAlidacoes para a criaçao de usuario e tabem colocar o username como a chave primaria PK

--- app/src/main/java/stp/vagnerjesus/mygestao/SignUp.java
+++ app/src/main/java/stp/vagnerjesus/mygestao/SignUp.java
@@ -25,12 +25,7 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
-
-
-
-
-        //Hooks to all xml elements in activity_sign_up.xml
+        //activity_sign_up.xml
         regName = findViewById(R.id.reg_name);
         regUsername = findViewById(R.id.reg_username);
         regEmail = findViewById(R.id.reg_email);
@@ -39,36 +34,116 @@ public class SignUp extends AppCompatActivity {
         regBtn = findViewById(R.id.reg_btn);
         regToLoginBtn = findViewById(R.id.reg_login_btn);
         //onCreate Method End
-
-
-        regBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {//onclik fire base
-                rootNode = FirebaseDatabase.getInstance();
-                reference = rootNode.getReference("user");
-
-                String name = regName.getEditText().getText().toString();
-                String username = regUsername.getEditText().getText().toString();
-                String email = regEmail.getEditText().getText().toString();
-                String phoneNo = regPhoneNo.getEditText().getText().toString();
-                String password = regPassword.getEditText().getText().toString();
-
-                UserHelperClass helperClass = new UserHelperClass(name,username,email,phoneNo,password);
-
-                reference.child(phoneNo).setValue(helperClass);
-
-            }
-        });
-
-
-
-
-
     }
 
     //button para ja tenho uma cota
     public void jatenho_conta(View view){
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+    //// Esta função será executada quando o usuário clicar no botão Register
+    public void registerUser(View view){
+
+        if(!validateName() | !validatePassword() | !validatePhoneNo() | !validateEmail() | !validateUsername()){
+            return;
+        }
+        rootNode = FirebaseDatabase.getInstance();
+        reference = rootNode.getReference("user");
+
+        String name = regName.getEditText().getText().toString();
+        String username = regUsername.getEditText().getText().toString();
+        String email = regEmail.getEditText().getText().toString();
+        String phoneNo = regPhoneNo.getEditText().getText().toString();
+        String password = regPassword.getEditText().getText().toString();
+
+        UserHelperClass helperClass = new UserHelperClass(name,username,email,phoneNo,password);
+
+        reference.child(username).setValue(helperClass);
+    }
+
+    ///Validacoes
+    private Boolean validateName() {
+        String val = regName.getEditText().getText().toString();
+
+        if (val.isEmpty()) {
+            regName.setError("O campo é obrigatório");
+            return false;
+        } else {
+            regName.setError(null);
+            regName.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private Boolean validateUsername() {
+        String val = regUsername.getEditText().getText().toString();
+        String noWhiteSpace = "\\A\\w{4,20}\\z";//para n ter espaços
+
+        if (val.isEmpty()) {
+            regUsername.setError("O campo é obrigatório");
+            return false;
+        } else if (val.length() >= 15) {
+            regUsername.setError("Nome de usuário muito longo");
+            return false;
+        } else if (!val.matches(noWhiteSpace)) {
+            regUsername.setError("Espaços em branco não são permitidos");
+            return false;
+        } else {
+            regUsername.setError(null);
+            regUsername.setErrorEnabled(false);
+            return true;
+        }
+    }
+    private Boolean validateEmail() {
+        String val = regEmail.getEditText().getText().toString();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (val.isEmpty()) {
+            regEmail.setError("O campo é obrigatório");
+            return false;
+        } else if (!val.matches(emailPattern)) {
+            regEmail.setError("Endereço de email invalido");
+            return false;
+        } else {
+            regEmail.setError(null);
+            regEmail.setErrorEnabled(false);
+            return true;
+        }
+    }
+    private Boolean validatePhoneNo() {
+        String val = regPhoneNo.getEditText().getText().toString();
+
+        if (val.isEmpty()) {
+            regPhoneNo.setError("O campo é obrigatório");
+            return false;
+        } else {
+            regPhoneNo.setError(null);
+            regPhoneNo.setErrorEnabled(false);
+            return true;
+        }
+    }
+    private Boolean validatePassword() {
+        String val = regPassword.getEditText().getText().toString();
+        String passwordVal = "^" +
+                //"(?=.*[0-9])" +         //pelo menos 1 dígito
+                //"(?=.*[a-z])" +         //pelo menos 1 letra minúscula
+                //"(?=.*[A-Z])" +         //pelo menos 1 letra maiúscula
+                "(?=.*[a-zA-Z])" +      //qualquer carta
+                "(?=.*[@#$%^&+=])" +    //pelo menos 1 caractere especial
+                "(?=\\S+$)" +           //sem espaços em branco
+                ".{4,}" +               //pelo menos 4 caracteres
+                "$";
+
+        if (val.isEmpty()) {
+            regPassword.setError("O campo é obrigatório");
+            return false;
+        } else if (!val.matches(passwordVal)) {
+            regPassword.setError("A senha é muito fraca");
+            return false;
+        } else {
+            regPassword.setError(null);
+            regPassword.setErrorEnabled(false);
+            return true;
+        }
     }
 }
